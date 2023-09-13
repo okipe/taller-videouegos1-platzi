8bb2729def6c6fa778782d064d985a1306ad3451
Oscar Roman <oscarroman.pe@gmail.com>
2023-09-13 16:53:32 -0500

    Refactor done

--- game.js
+++ game.js
@@ -5,12 +5,14 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame) //apenas abra, se ejecuará
+let canvasSize; 
+let elementsSize;
 
-// Para usarla en el futuro
-function startGame() {
+window.addEventListener('load', setCanvasSize); //apenas abra, se ejecuará el size
+window.addEventListener('resize', setCanvasSize); // se hará resize de manera dinámica 
+
+function setCanvasSize() {
     // Determinamos el tamaño de la ventana.
-    let canvasSize; 
     if(window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.8;  // Pantalla con 75% responsive al abrir o recargar
     } else {
@@ -21,19 +23,45 @@ function startGame() {
     canvas.setAttribute('height', canvasSize); 
     
     // Elementos del tablero
-    const elementsSize = (canvasSize / 10) - 1;
+    elementsSize = (canvasSize / 10) - 1;
+
+    startGame()
+
+function startGame () {
     console.log({canvasSize, elementsSize});
 
     game.font = elementsSize + 'px Verdana';
-    game.textAlign = '';
+    game.textAlign = 'end';
 
-    // Rellenando con bombitas
-    for (let i = 0; i < 10; i++) {
-        game.fillText(emojis['X'], elementsSize * i, elementsSize);
-        
-    }
+    // Extraigo los caracteres de maps.js
+    const map = maps[0];
+    // Creo las filas. Trim para limpiar los spacios, split para crear arreglos desde strings
+    const mapRows = map.trim().split('\n');
+    // Creo las columnas
+    const mapRowCols = mapRows.map(row => row.trim().split(''));
+    console.log({map, mapRows, mapRowCols});
 
-    
+    mapRowCols.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementsSize * (colI + 1);
+            const posY = elementsSize * (rowI + 1);
+            game.fillText(emoji, posX, posY);
+        })
+    });
+
+    /* // Rellenando con bombitas
+    for (let row = 1; row <= 10; row++) {
+        // console.log(row);
+        for (let col = 1; col <= 10; col++) {
+            game.fillText(emojis[mapRowCols[row - 1][col - 1]],
+                (elementsSize * col) + 12, (elementsSize * row) -1);
+            // console.log(col);
+        }
+    } */
+}
+
+    // Me quedé en el minuto 5:19, video 6.
     
     // game.fillRect(0,0,100,100); // Dibuja trazo (x,y, hasta donde llega x, hasta donde llega y)
     // game.clearRect(0,0,100,100); // Borra (mismos que el anterior)
